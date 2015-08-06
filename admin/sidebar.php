4cb58be97afbbe1c13a5a97b2002036f6a57ddf2
James Montgomery <monty@wpmonty.com>
2015-08-06 11:01:59 -0400

    fixed sidebar, added feed burner
    * ADDED: new feed burner link
    * IMPROVED: sidebar modules

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,5 +1,50 @@
 <div id="wpsite_plugin_sidebar" class="metabox-holder col-md-3">
 
+    <div class="postbox">
+		<h3><?php _e('Subscribe to Our Newsletter', self::$text_domain); ?></h3>
+		<div class="inside">
+    		<p><?php _e('Signup now to get notified of plugin updates, awesome themes, and more. Over 17,000 already have:', self::$text_domain); ?></p>
+			<!-- Begin MailChimp Signup Form -->
+			<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+			<style type="text/css">
+				#mc_embed_signup {
+					background:#fff;
+					clear:left;
+					font:14px Helvetica,Arial,sans-serif;
+				}
+				/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+				We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+			</style>
+			<div id="mc_embed_signup">
+			<form action="//wpsite.us5.list-manage.com/subscribe/post?u=82c2341134bbdc37714642adb&amp;id=642b18616e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<div id="mc_embed_signup_scroll">
+					<div class="mc-field-group">
+						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="<?php _e('Email Address', self::$text_domain); ?>">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;"><input type="text" name="b_82c2341134bbdc37714642adb_642b18616e" tabindex="-1" value=""></div>
+					<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn-primary btn"></div>
+				</div>
+			</form>
+			</div>
+			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			<!--End mc_embed_signup-->
+		</div>
+	</div>
+
+	<div class="postbox">
+		<h3><?php _e('Must-Read Articles', self::$text_domain); ?></h3>
+		<div class="inside">
+			<div class="wpsite_feed">
+				<script src="http://feeds.feedburner.com/99Robots?format=sigpro" type="text/javascript" ></script><noscript><p><?php _e('Subscribe to 99Robots Feed:', self::$text_domain); ?> <a href="http://feeds.feedburner.com/99Robots"></a><br/><?php _e('Powered by FeedBurner', self::$text_domain); ?></p> </noscript>
+			</div>
+		</div>
+	</div>
+
+<!--
 	<div class="">
 		<h3><?php _e('Need Plugin or Theme Development?', self::$text_domain); ?></h3>
 			<div class="inside">
@@ -24,5 +69,6 @@
 			<a class="btn btn-success" href="https://99robots.com/support">Get Support</a>
 		</div>
 	</div>
+-->
 
 </div> <!-- wpsite_plugin_sidebar -->
